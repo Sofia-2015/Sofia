48f30a296fc66ac5e5ce4d309a43718addd2a034
johnjordancsc <john@cscbeyond.com>
2016-01-02 14:58:56 -0600

    Create frmLog.cs

---
+++ WindowsFormsApplication1/frmLog.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmLog : Form
+    {
+        public frmLog()
+        {
+            InitializeComponent();
+            Logging.loadLog();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Logging.exportExcel();
+        }
+    }
+}
